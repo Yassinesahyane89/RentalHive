2d32f71d7312352f12f25ff8ca74511c6745f5a1
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-11-20 09:56:00 +0100

    Initial commit

---
+++ src/main/java/com/example/lfri3/Lfri3Application.java
@@ -0,0 +1,13 @@
+package com.example.lfri3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lfri3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lfri3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lfri3/Lfri3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lfri3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lfri3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
