02bd70d7ff73ae139ece8676ce4924e81e1da8fe
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-11-21 03:29:06 +0100

    add Customer entity with validation

---
+++ src/main/java/com/example/lfri3/entity/Customer.java
@@ -0,0 +1,27 @@
+package com.example.lfri3.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+}
